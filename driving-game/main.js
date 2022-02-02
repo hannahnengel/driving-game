464277a37b0b7f35f4ecae7c734b69014c5acf14
Hannah Engelhardt <hannahnengel@gmail.com>
2022-02-02 00:04:02 -0800

    Create a checkCrash function that checks if the user's car crashes into enemy cars, if so, crash / reset the game #6

--- driving-game/main.js
+++ driving-game/main.js
@@ -16,6 +16,30 @@ $enemyCar1.style.left = leftScreenX.toString() + 'px';
 $finishLine.style.left = leftScreenX.toString() + 'px';
 $finishLine.style.top = topScreenY.toString() + 'px';
 
+var enemyCars = {
+  enemyCar1: {
+    direction: 'West',
+    location: {
+      x: leftScreenX,
+      y: 300
+    }
+  },
+  enemyCar2: {
+    direction: 'East',
+    location: {
+      x: 15,
+      y: 600
+    }
+  }
+};
+
+// var finishLine = {
+//   location: {
+//     x: leftScreenX,
+//     y: topScreenY
+//   }
+// };
+
 var $carImg = document.querySelector('.car-img');
 var myInt;
 
@@ -42,66 +66,59 @@ function moveCar(event) {
 
       car.location.x = parseInt($carImg.style.left);
       if (isNaN(parseInt($carImg.style.left))) {
-        car.location.x = 0;
+        car.location.x = 8;
       }
       car.location.y = parseInt($carImg.style.top);
       if (isNaN(parseInt($carImg.style.top))) {
-        car.location.y = 0;
+        car.location.y = 8;
       }
 
       car.start = false;
     }
 
   }
+}
 
-  function startCar() {
-    car.start = true;
-    var pixelsToMove;
-    var newValue;
-    var movementSpeed = 10;
+function startCar() {
+  car.start = true;
+  var pixelsToMove;
+  var newValue;
+  var movementSpeed = 10;
 
-    if (car.direction === 'East') {
-      pixelsToMove = parseInt($carImg.style.left) + movementSpeed;
-      if (isNaN(pixelsToMove)) {
-        pixelsToMove = movementSpeed;
-      }
-      newValue = pixelsToMove.toString();
-      car.location.x = newValue;
-      $carImg.style.left = (newValue + 'px');
-    } else if (car.direction === 'West') {
-      pixelsToMove = parseInt($carImg.style.left) - movementSpeed;
-      if (isNaN(pixelsToMove)) {
-        pixelsToMove = -movementSpeed;
-      }
-      newValue = pixelsToMove.toString();
-      car.location.x = newValue;
-      $carImg.style.left = (newValue + 'px');
-    } else if (car.direction === 'South') {
-      pixelsToMove = parseInt($carImg.style.top) + movementSpeed;
-      if (isNaN(pixelsToMove)) {
-        pixelsToMove = movementSpeed;
-      }
-      newValue = pixelsToMove.toString();
-      car.location.y = newValue;
-      $carImg.style.top = (newValue + 'px');
-    } else if (car.direction === 'North') {
-      pixelsToMove = parseInt($carImg.style.top) - movementSpeed;
-      if (isNaN(pixelsToMove)) {
-        pixelsToMove = -movementSpeed;
-      }
-      newValue = pixelsToMove.toString();
-      car.location.y = newValue;
-      $carImg.style.top = (newValue + 'px');
+  if (car.direction === 'East') {
+    pixelsToMove = parseInt($carImg.style.left) + movementSpeed;
+    if (isNaN(pixelsToMove)) {
+      pixelsToMove = movementSpeed;
     }
-
-    var $crash = document.querySelector('.crash');
-    if (car.location.x <= -1 || car.location.y <= -1 || car.location.x >= leftScreenX + 15 || car.location.y >= topScreenY + 15) {
-      $crash.classList.remove('hidden');
-      clearInterval(myInt);
-      setTimeout(resetGame, 1500);
+    newValue = pixelsToMove.toString();
+    car.location.x = newValue;
+    $carImg.style.left = (newValue + 'px');
+  } else if (car.direction === 'West') {
+    pixelsToMove = parseInt($carImg.style.left) - movementSpeed;
+    if (isNaN(pixelsToMove)) {
+      pixelsToMove = -movementSpeed;
     }
+    newValue = pixelsToMove.toString();
+    car.location.x = newValue;
+    $carImg.style.left = (newValue + 'px');
+  } else if (car.direction === 'South') {
+    pixelsToMove = parseInt($carImg.style.top) + movementSpeed;
+    if (isNaN(pixelsToMove)) {
+      pixelsToMove = movementSpeed;
+    }
+    newValue = pixelsToMove.toString();
+    car.location.y = newValue;
+    $carImg.style.top = (newValue + 'px');
+  } else if (car.direction === 'North') {
+    pixelsToMove = parseInt($carImg.style.top) - movementSpeed;
+    if (isNaN(pixelsToMove)) {
+      pixelsToMove = -movementSpeed;
+    }
+    newValue = pixelsToMove.toString();
+    car.location.y = newValue;
+    $carImg.style.top = (newValue + 'px');
   }
-
+  checkCrash();
 }
 
 function resetGame() {
@@ -118,4 +135,53 @@ function resetGame() {
   $carImg.style.left = '8px';
   $carImg.style.top = '8px';
   $carImg.className = 'car-img east';
+}
+
+function checkCrash() {
+  var crash = false;
+
+  var carLeftX = parseInt(car.location.x);
+  var carRightX = parseInt(car.location.x) + 150;
+  var carTopY = parseInt(car.location.y);
+  var carBottomY = parseInt(car.location.y) + 150;
+
+  var enemyCar1LeftX = parseInt(enemyCars.enemyCar1.location.x);
+  var enemyCar1RightX = parseInt(enemyCars.enemyCar1.location.x) + 150;
+  var enemyCar1TopY = parseInt(enemyCars.enemyCar1.location.y);
+  var enemyCar1BottomY = parseInt(enemyCars.enemyCar1.location.y) + 105;
+
+  var enemyCar2LeftX = parseInt(enemyCars.enemyCar2.location.x);
+  var enemyCar2RightX = parseInt(enemyCars.enemyCar2.location.x) + 150;
+  var enemyCar2TopY = parseInt(enemyCars.enemyCar2.location.y);
+  var enemyCar2BottomY = parseInt(enemyCars.enemyCar2.location.y) + 105;
+
+  if (carLeftX > enemyCar2RightX || carRightX < enemyCar2LeftX) {
+    crash = false;
+  } else if (carTopY > enemyCar2BottomY || carBottomY < enemyCar2TopY) {
+    crash = false;
+  } else {
+    crash = true;
+  }
+
+  if (crash === false) {
+    if ((carLeftX > enemyCar1RightX || carRightX < enemyCar1LeftX)) {
+      crash = false;
+    } else if (carTopY > enemyCar1BottomY || carBottomY < enemyCar1TopY) {
+      crash = false;
+    } else {
+      crash = true;
+    }
+  }
+
+  if (car.location.x <= -1 || car.location.y <= -1 || car.location.x >= leftScreenX + 15 || car.location.y >= topScreenY + 15) {
+    crash = true;
+  }
+
+  if (crash) {
+    var $crash = document.querySelector('.crash');
+    $crash.classList.remove('hidden');
+    clearInterval(myInt);
+    setTimeout(resetGame, 1500);
+    crash = false;
+  }
 }
